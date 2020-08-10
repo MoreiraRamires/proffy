ee0476f406bbaf04e8c7fe15606e164baa435a8c
MoreiraRamires <62817742+MoreiraRamires@users.noreply.github.com>
2020-08-10 14:47:27 -0300

    server-refatorado

--- src/server.js
+++ src/server.js
@@ -26,29 +26,25 @@ const proffys = [
 
 ]
 
+function pageLanding(req, res) {
+  return res.sendFile(__dirname + "/views/index.html")
+}
+function pageStudy(req, res) {
+  return res.sendFile(__dirname + "/views/study.html")
+}
+function pageGiveClasses(req, res) {
+  return res.sendFile(__dirname + "/views/give-classes.html")
+}
 
 const express = require('express')
 const server = express()
 
 
 server.use(express.static("public"))
-  .get("/", (req, res) => {
-    return res.sendFile(__dirname + "/views/index.html")
-
-  })
-
-  .get("/study", (req, res) => {
-    return res.sendFile(__dirname + "/views/study.html")
-
-  })
-
-  .get("/give-classes", (req, res) => {
-    return res.sendFile(__dirname + "/views/give-classes.html")
-
-  })
+  .get("/", pageLanding)
+  .get("/study", pageStudy)
+  .get("/give-classes", pageGiveClasses)
 
 
-  .get("/study", (req, res) => {
-    return res.send("Study")
-  })
+
   .listen(5000)
